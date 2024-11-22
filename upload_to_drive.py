5da5de10932795437aa6bc20428cdfa984ec7ff2
Octavio Junior <octaviopcjunior@hotmail.com>
2024-11-21 23:27:42 -0300

    fix: routes files

--- upload_to_drive.py
+++ upload_to_drive.py
@@ -1,10 +1,10 @@
 import logging
 import pickle
+import os
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
-import os
 
 
 def upload_to_drive(file_path, file_name):
@@ -13,18 +13,33 @@ def upload_to_drive(file_path, file_name):
     SCOPES = ["https://www.googleapis.com/auth/drive.file"]
     creds = None
 
-    if os.path.exists("token.pickle"):
-        with open("token.pickle", "rb") as token:
+    # Caminhos relativos para os arquivos de credenciais e token
+    base_path = os.path.dirname(__file__)
+    credentials_path = os.path.join(base_path, "credentials.json")
+    token_path = os.path.join(base_path, "token.pickle")
+
+    # Verificar se os arquivos necessários existem
+    if not os.path.exists(credentials_path):
+        logging.error(f"Arquivo de credenciais {credentials_path} não encontrado!")
+        return  # Interrompe a execução se o arquivo não for encontrado
+
+    if not os.path.exists(token_path):
+        logging.error(f"Arquivo de token {token_path} não encontrado!")
+        return  # Interrompe a execução se o arquivo não for encontrado
+
+    # Carregar o token se ele existir
+    if os.path.exists(token_path):
+        with open(token_path, "rb") as token:
             creds = pickle.load(token)
 
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file(credentials_path, SCOPES)
             creds = flow.run_local_server(port=0)
 
-        with open("token.pickle", "wb") as token:
+        with open(token_path, "wb") as token:
             pickle.dump(creds, token)
 
     service = build("drive", "v3", credentials=creds)
